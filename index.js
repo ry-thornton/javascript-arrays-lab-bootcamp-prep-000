1bc960e5254ad2326a9a0a662e84f30cc02d412f
ryanthornton628 <ryanthornton628@gmail.com>
2019-06-04 19:41:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length-1)
+  kittens.slice(0, kittens.length - 1)
 }
 // Add your functions and code here
